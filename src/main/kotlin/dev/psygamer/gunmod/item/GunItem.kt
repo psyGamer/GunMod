c3204d1542c7c7732b0a473ef3a95008fe7ccd36
psyGamer <56753271+psyGamer@users.noreply.github.com>
2022-02-22 18:30:48 +0100

    :fire: Removed item modifiers from `GunItem`

--- src/main/kotlin/dev/psygamer/gunmod/item/GunItem.kt
+++ src/main/kotlin/dev/psygamer/gunmod/item/GunItem.kt
@@ -1,15 +1,9 @@
 package dev.psygamer.gunmod.item
 
-import com.google.common.collect.*
-import net.minecraft.world.entity.EquipmentSlot
-import net.minecraft.world.entity.ai.attributes.*
 import net.minecraft.world.item.Item
-import java.util.*
 import dev.psygamer.gunmod.GunMod
 
 class GunItem(properties: Properties) : Item(properties) {
-	
-	private val defaultModifiers: Multimap<Attribute, AttributeModifier>
 	
 	companion object {
 		
@@ -19,16 +13,5 @@ class GunItem(properties: Properties) : Item(properties) {
 	
 	init {
 		this.setRegistryName(GunMod.MODID, "gunitem")
-		
-		// Apply speed modifier when holding the gun
-		val builder = ImmutableMultimap.builder<Attribute, AttributeModifier>()
-		builder.put(Attributes.MOVEMENT_SPEED, AttributeModifier(SPEED_MODIFIER_HOLDING_UUID, "Speed modifier", 0.8,
-																 AttributeModifier.Operation.MULTIPLY_TOTAL))
-		@Suppress("UNCHECKED_CAST") // WTF Kotlin
-		this.defaultModifiers = builder.build() as Multimap<Attribute, AttributeModifier>
-	}
-	
-	override fun getDefaultAttributeModifiers(pSlot: EquipmentSlot): Multimap<Attribute, AttributeModifier> {
-		return defaultModifiers
 	}
 }
