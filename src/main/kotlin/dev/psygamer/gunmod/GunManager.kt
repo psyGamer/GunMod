f311242f1844589a45ffc5ad2e233de8dd29bae2
psyGamer <56753271+psyGamer@users.noreply.github.com>
2022-02-22 18:32:17 +0100

    :art: Improved speed modifier application

--- src/main/kotlin/dev/psygamer/gunmod/GunManager.kt
+++ src/main/kotlin/dev/psygamer/gunmod/GunManager.kt
@@ -2,8 +2,6 @@ package dev.psygamer.gunmod
 
 import net.minecraft.client.Minecraft
 import net.minecraft.client.player.LocalPlayer
-import net.minecraft.world.entity.ai.attributes.*
-import net.minecraft.world.entity.player.Player
 import net.minecraftforge.client.event.*
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
@@ -11,6 +9,7 @@ import org.lwjgl.glfw.GLFW.*
 import dev.psygamer.gunmod.item.GunItem
 import dev.psygamer.gunmod.network.send
 import dev.psygamer.gunmod.network.to_server.SShootWeaponPacket
+import dev.psygamer.gunmod.registry.ItemRegistry
 
 @Mod.EventBusSubscriber
 object GunManager {
@@ -35,7 +34,7 @@ object GunManager {
 		if (event.button == GLFW_MOUSE_BUTTON_1 && event.action == GLFW_PRESS)
 			fire(player)
 		else if (event.button == GLFW_MOUSE_BUTTON_2)
-			handleAiming(player, event)
+			handleAiming(event)
 		
 		event.isCanceled = event.action == GLFW_PRESS && event.button == GLFW_MOUSE_BUTTON_1
 	}
@@ -49,20 +48,26 @@ object GunManager {
 				event.fov
 	}
 	
-	private fun handleAiming(player: Player, event: InputEvent.RawMouseEvent) {
-		if (event.action == GLFW_PRESS) {
-			if (player.attributes.hasModifier(Attributes.MOVEMENT_SPEED, GunItem.SPEED_MODIFIER_AIMING_UUID))
-				return
-			
-			aiming = true
-			player.attributes.getInstance(Attributes.MOVEMENT_SPEED)
-					?.addTransientModifier(AttributeModifier(GunItem.SPEED_MODIFIER_AIMING_UUID, "Speed modifier", 0.5,
-															 AttributeModifier.Operation.MULTIPLY_TOTAL))
-		} else if (event.action == GLFW_RELEASE) {
-			aiming = false
-			player.attributes.getInstance(Attributes.MOVEMENT_SPEED)
-					?.removeModifier(GunItem.SPEED_MODIFIER_AIMING_UUID)
+	@SubscribeEvent
+	fun onMovementInputUpdate(event: MovementInputUpdateEvent) {
+		if (aiming) {
+			event.input.forwardImpulse *= GunItem.AIMING_SPEED_MODIFIER
+			event.input.leftImpulse *= GunItem.AIMING_SPEED_MODIFIER
+			return // Only apply 1 modifier
 		}
+		
+		// Check if holding
+		if (event.player.mainHandItem.item == ItemRegistry.GUN_ITEM) {
+			event.input.forwardImpulse *= GunItem.HOLDING_SPEED_MODIFIER
+			event.input.leftImpulse *= GunItem.HOLDING_SPEED_MODIFIER
+		}
+	}
+	
+	private fun handleAiming(event: InputEvent.RawMouseEvent) {
+		if (event.action == GLFW_PRESS)
+			aiming = true
+		else if (event.action == GLFW_RELEASE)
+			aiming = false
 	}
 	
 	private fun fire(player: LocalPlayer) {
--- src/main/kotlin/dev/psygamer/gunmod/item/GunItem.kt
+++ src/main/kotlin/dev/psygamer/gunmod/item/GunItem.kt
@@ -7,8 +7,10 @@ class GunItem(properties: Properties) : Item(properties) {
 	
 	companion object {
 		
-		val SPEED_MODIFIER_HOLDING_UUID: UUID = UUID.fromString("e8dcdb26-9037-11ec-b909-0242ac120002")
-		val SPEED_MODIFIER_AIMING_UUID: UUID = UUID.fromString("b755fde3-c377-4d5c-8773-986f33fc0e08")
+		// TODO: Make this dynamic based on the gun
+		const val FOV_MODIFIER = 0.1f
+		const val HOLDING_SPEED_MODIFIER = 0.8f
+		const val AIMING_SPEED_MODIFIER = 0.2f
 	}
 	
 	init {
